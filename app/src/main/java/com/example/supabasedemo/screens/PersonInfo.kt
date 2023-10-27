2321d777e171e1055526f27a5f9bd59e32f1e7f9
danya <danyapancresnicov@gmail.com>
2023-10-27 19:19:13 +0300

    Todo: Дописать допинфу о person

---
+++ app/src/main/java/com/example/supabasedemo/screens/PersonInfo.kt
@@ -0,0 +1,12 @@
+package com.example.supabasedemo.screens
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.example.supabasedemo.viewmodel.PersonInfoViewmodel
+
+@Composable
+fun PersonInfoScreen(lesson: Int, navController: NavController, viewModel: PersonInfoViewmodel =
+{
+
+}
